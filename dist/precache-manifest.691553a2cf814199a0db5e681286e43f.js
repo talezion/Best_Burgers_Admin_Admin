8cf1abf69b344c9e72d1caf2deca771c866f5753
Salman <sajidsalman75@gmail.com>
2019-08-29 19:32:24 +0500

    Distribution folder added

---
+++ dist/precache-manifest.691553a2cf814199a0db5e681286e43f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "21418673284c16ce1bdc",
+    "url": "/js/chunk-vendors.d160b283.js"
+  },
+  {
+    "revision": "da6c16318ed51a86f7d2",
+    "url": "/js/app.eb2affd8.js"
+  },
+  {
+    "revision": "2e055c639f54f63f52e27fddd94b9216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "38e644786b2428b10b0ee3c9ba384eea",
+    "url": "/img/burger1.38e64478.jpg"
+  },
+  {
+    "revision": "21418673284c16ce1bdc",
+    "url": "/css/chunk-vendors.ee7084df.css"
+  }
+];
