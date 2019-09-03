c6223c1bb8d50b3acbbd88b75fff7156ea408f53
Salman <salman@onsets.co>
2019-09-03 14:19:04 +0500

    Burger thumbnail/images size fixed

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="type=='thumbnail' && images.length > 0">
+  <div v-if="type == 'thumbnail' && images.length > 0">
     <b-carousel
       id="carousel-1"
       v-model="slide"
@@ -11,18 +11,23 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <b-carousel-slide img-height="200" img-width="200" v-for="(approved_image, index) in approved_images_container" :key="index">
+      <b-carousel-slide
+        img-height="200"
+        img-width="200"
+        v-for="(approved_image, index) in approved_images_container"
+        :key="index"
+      >
         <img
           slot="img"
-          class="d-block img-fluid mx-auto"
+          class="d-block img-fluid mx-auto wh"
           :src="getImageUrl(index)"
           alt="image slot"
-        >
-      </b-carousel-slide>
-    </b-carousel><br>
+        />
+      </b-carousel-slide> </b-carousel
+    ><br />
     <button class="btn btn-primary btn-block" @click="setClicked">Set</button>
   </div>
-  <div v-else-if="type=='images' && images.length > 0">
+  <div v-else-if="type == 'images' && images.length > 0">
     <b-carousel
       id="carousel-1"
       v-model="slide"
@@ -34,64 +39,79 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <b-carousel-slide img-height="200" img-width="200" v-for="(approved_image, index) in approved_images_container" :key="index">
+      <b-carousel-slide
+        img-height="200"
+        img-width="200"
+        v-for="(approved_image, index) in approved_images_container"
+        :key="index"
+      >
         <img
           slot="img"
-          class="d-block img-fluid mx-auto"
+          class="d-block img-fluid mx-auto wh"
           :src="getImageUrl(index)"
           alt="image slot"
-        >
-      </b-carousel-slide>
-    </b-carousel><br>
+        />
+      </b-carousel-slide> </b-carousel
+    ><br />
     <div class="d-flex">
-      <button class="btn btn-primary flex-fill" @click="approveClicked">Approve</button>
-      <button class="btn btn-danger flex-fill" @click="rejectClicked">Reject</button>
+      <button class="btn btn-primary flex-fill" @click="approveClicked">
+        Approve
+      </button>
+      <button class="btn btn-danger flex-fill" @click="rejectClicked">
+        Reject
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import {images_approved, images, images_container, burgersRef} from "../firebase";
+import {
+  images_approved,
+  images,
+  images_container,
+  burgersRef
+} from "../firebase";
 export default {
   name: "carousel",
-  props: ['image_key', 'type'],
-  data: function(){
-    return{
+  props: ["image_key", "type"],
+  data: function() {
+    return {
       slide: 0,
       sliding: null,
       images_ids: [],
       images: [],
       approved_images_ids: [],
       current_images: [],
-      approved_images_container: '',
-      container_images: [],
-    }
+      approved_images_container: "",
+      container_images: []
+    };
   },
   firebase: {
     approved_images_ids: images_approved.orderByKey(),
     images: images.orderByKey(),
-    container_images: images_container.orderByKey(),
+    container_images: images_container.orderByKey()
   },
   methods: {
     onSlideStart(slide) {
-      this.sliding = true
+      this.sliding = true;
     },
     onSlideEnd(slide) {
-      this.sliding = false
+      this.sliding = false;
     },
-    getImageUrl: function(key){
+    getImageUrl: function(key) {
       let imageIndex = -1;
-      for(let i = 0 ; i < this.images.length ; i++){
-        if(this.images[i]['.key'] == key){
+      for (let i = 0; i < this.images.length; i++) {
+        if (this.images[i][".key"] == key) {
           imageIndex = i;
           break;
         }
       }
       return this.images[imageIndex].url;
     },
-    approveClicked: function(){
-      
-      let properties = Object.getOwnPropertyNames(this.container_images[this.slide]);
+    approveClicked: function() {
+      let properties = Object.getOwnPropertyNames(
+        this.container_images[this.slide]
+      );
       let key = properties[0];
       console.log(this.container_images[this.slide]);
       //this.approved_images_container.push(this.container_images[this.slide]);
@@ -99,30 +119,42 @@ export default {
       //images_approved.child(this.image_key).push({key: 1});
       //images_container.child(this.image_key).remove(this.container_images[this.slide]);
     },
-    rejectClicked: function(){
-      burgersRef.child(this.image_key).update({was_reviewed: true, is_validated: true});
+    rejectClicked: function() {
+      burgersRef
+        .child(this.image_key)
+        .update({ was_reviewed: true, is_validated: true });
     },
-    setClicked: function(){
+    setClicked: function() {
       console.log(this.images[this.slide]);
-      burgersRef.child(this.image_key).update({highlight_image: this.images[this.slide]});
+      burgersRef
+        .child(this.image_key)
+        .update({ highlight_image: this.images[this.slide] });
     }
   },
-  mounted(){
-    if(this.type == 'thumbnail'){
-      this.approved_images_ids.forEach((element) => {
-        if(element['.key'] == this.image_key){
+  mounted() {
+    if (this.type == "thumbnail") {
+      this.approved_images_ids.forEach(element => {
+        if (element[".key"] == this.image_key) {
           this.approved_images_container = element;
         }
       });
-    }
-    else{
-      this.container_images.forEach((element) => {
-        if(element['.key'] == this.image_key){
-          this.approved_images_container = images_container.orderByKey().equalTo(this.image_key); 
+    } else {
+      this.container_images.forEach(element => {
+        if (element[".key"] == this.image_key) {
+          this.approved_images_container = images_container
+            .orderByKey()
+            .equalTo(this.image_key);
           //this.approved_images_container = element;
         }
       });
     }
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.wh {
+  height: 200px;
+  width: 100%;
+}
+</style>
