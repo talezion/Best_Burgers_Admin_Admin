4c0f4f500db0a9afbcd18f838178eec6238b9a21
Salman <sajidsalman75@gmail.com>
2019-09-03 10:55:54 +0500

    Login loading animation added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container-fluid">
     <div class="row text-center" style="margin-top:10%">
-      <div class="col-md-4"></div>
+      <div class="col-md-4">
+      </div>
       <div class="col-md-4 col-sm-12 align-self-center">
         <h3 class="mb-5">Best Burgers Admin Login</h3>
         <form @submit="loginClicked">
@@ -14,27 +15,38 @@
           <input type="submit" value="Login" class="btn btn-primary btn-block" />              
         </form>
       </div>
+      <div class="col-md-12" v-if="showAnimation">
+        <br>
+        <LoadingAnimation />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
+import LoadingAnimation from "@/components/LoadingAnimation";
 
 export default {
   name: "login",
+  components: {
+    LoadingAnimation,
+  },
   data: function(){
     return{
       email: '',
       password: '',
+      showAnimation: false,
     }
   },
   methods: {
     loginClicked: function(e){
       e.preventDefault();
       let comp = this;
+      this.showAnimation = true;
       firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
         (user) => {
+          this.showAnimation = false;
           this.$router.push('home');
         },
         (err) => {
